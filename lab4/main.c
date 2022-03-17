d33393b6ddaf5abb0d08b574afcd2b7e77fc22fe
Abram Grosman <54711944+Marba1805@users.noreply.github.com>
2022-03-17 16:27:47 -0300

    Create main.c

---
+++ lab4/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <cstdlib>
+
+struct PESSOA{
+    char Nome;
+    int idade;
+    int altura;
+}
+
+int main()
+{
+     char S1[50];
+     PESSOA Povo[10], P;
+     FILE *Arq;
+     int i;
+
+     leNomeArquivoEntrada(S1);
+     if (abreArquivoAEntrada(Arq, S1) == 1)
+     {
+            do{
+                    if (leDadosUmaPessoa(P)== 1)
+                    {
+                        Povo[i] = P;
+                        i++;
+                    }
+                    else break;
+            } while(1);
+            fechaArquivo(Arq);
+            ordenaVetor(Povo, i);
+            imprimeVetor(Povo,i);
+    }
+    else printf("Erro na abertura do arquivo");
+     return 0;
+
+}
